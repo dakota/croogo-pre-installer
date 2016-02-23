f00adfbe6b0fa3753b49a0ecc344b623200c1d9a
Walther Lalk <waltherlalk@gmail.com>
2016-02-23 13:55:51 +0200

    Fix package installer

--- webroot/installer/scripts/installPackage.php
+++ webroot/installer/scripts/installPackage.php
@@ -4,7 +4,7 @@ require "CroogoInstaller.php";
 $package = $_GET['package'];
 $version = $_GET['version'];
 
-$allowedPackages = json_decode(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'dependencies.json', true);
+$allowedPackages = json_decode(file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'dependencies.json'), true);
 
 if (!isset($allowedPackages[$package])) {
     echo '0';
@@ -13,4 +13,6 @@ if (!isset($allowedPackages[$package])) {
 
 $croogoInstaller = new CroogoInstaller();
 
-$croogoInstaller->installPackage($package, $version);
+$output = $croogoInstaller->installPackage($package, $version);
+
+echo $output;
